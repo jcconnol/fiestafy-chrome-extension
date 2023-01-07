e37f0cbcf3919edcfd574f44cb047ca74294c71e
John Connolly <jcconnol4@gmail.com>
2023-01-06 23:56:16 -0600

    work on some valentines changes

--- content.js
+++ content.js
@@ -121,21 +121,98 @@ function runValentines(){
     document.body.appendChild(newDiv);
 
     if (holidayRange >= 1) {
-        christmasTreeImagePath = getExtensionURL('valentines/christmas-tree.png')
+        arrowHeartImagePath = getExtensionURL('valentines/arrow-heart.png')
         document.getElementById("valentines-images-container").innerHTML += 
-            "<img src=\"" + christmasTreeImagePath + "\" id=\"christmas-tree-image\">"
+            "<img src=\"" + arrowHeartImagePath + "\" id=\"arrow-heart-image\">"
+
+        heartBalloonImagePath = getExtensionURL('valentines/heart-balloons.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + heartBalloonImagePath + "\" id=\"heart-balloon-image\">"
+    }
+
+    if (holidayRange >= 2) {
+        heartBannerImagePath = getExtensionURL('valentines/heart-banner.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<div id=\"heart-banner-image\"></div>"
+        document.getElementById("heart-banner-image").style.backgroundImage = "url(" + heartBannerImagePath + ")"
+    }
+
+    if (holidayRange >= 5){
+        arrowImagePath = getExtensionURL('valentines/arrow.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + arrowImagePath + "\" id=\"arrow-image\">"
+    
+        
+
+        
+
+        heartGardenImagePath = getExtensionURL('valentines/heart-garden.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + heartGardenImagePath + "\" id=\"heart-garden-image\">"
+
+        heartGunImagePath = getExtensionURL('valentines/heart-gun.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + heartGunImagePath + "\" id=\"heart-gun-image\">"
+
+        heartHotAirImagePath = getExtensionURL('valentines/heart-hot-air.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + heartHotAirImagePath + "\" id=\"heart-hot-air-image\">"
+
+        heartLetterImagePath = getExtensionURL('valentines/heart-letter.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + heartLetterImagePath + "\" id=\"heart-letter-image\">"
+
+        milkShakeImagePath = getExtensionURL('valentines/milk-shake.png')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + milkShakeImagePath + "\" id=\"milk-shake-image\">"
+
+        valentinesDayImagePath = getExtensionURL('valentines/valentines-day.gif')
+        document.getElementById("valentines-images-container").innerHTML += 
+            "<img src=\"" + valentinesDayImagePath + "\" id=\"valentines-day-image\">"
     }
 }
 
 function runIndependenceDay(){
     
     var newDiv = document.createElement("DIV"); 
-    newDiv.setAttribute("id", "valentines-images-container");
+    newDiv.setAttribute("id", "independence-images-container");
     document.body.appendChild(newDiv);
 
     if (holidayRange >= 1) {
-        christmasTreeImagePath = getExtensionURL('valentines/christmas-tree.png')
-        document.getElementById("valentines-images-container").innerHTML += 
-            "<img src=\"" + christmasTreeImagePath + "\" id=\"christmas-tree-image\">"
+        fireworkImagePath = getExtensionURL('independence-day/firework.png')
+        document.getElementById("independence-images-container").innerHTML += 
+            "<img src=\"" + fireworkImagePath + "\" id=\"firework-image\">"
     }
+
+    firework2ImagePath = getExtensionURL('independence-day/firework-2.png')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + fireworkImagePath + "\" id=\"firework-2-image\">"
+
+    goldBannerImagePath = getExtensionURL('independence-day/gold-banner.png')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + goldBannerImagePath + "\" id=\"gold-banner-image\">"
+
+    kiteImagePath = getExtensionURL('independence-day/kite.png')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + kiteImagePath + "\" id=\"kite-image\">"
+
+    sparkePlumeImagePath = getExtensionURL('independence-day/sparkle-plume.png')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + sparkePlumeImagePath + "\" id=\"sparkle-plume-image\">"
+
+    sparklerImagePath = getExtensionURL('independence-day/sparkler.gif')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + sparklerImagePath + "\" id=\"sparkler-image\">"
+
+    spiralImagePath = getExtensionURL('independence-day/spiral.png')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + spiralImagePath + "\" id=\"spiral-image\">"
+
+    usaBannerImagePath = getExtensionURL('independence-day/usa-banner.png')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + usaBannerImagePath + "\" id=\"usa-banner-image\">"
+
+    wavingFlagImagePath = getExtensionURL('independence-day/waving-flag.png')
+    document.getElementById("independence-images-container").innerHTML += 
+        "<img src=\"" + wavingFlagImagePath + "\" id=\"waving-flag-image\">"
 }
